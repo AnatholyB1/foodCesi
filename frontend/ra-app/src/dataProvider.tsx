248cab38e6e44c5458ebf2561e0346e5fb6b5256
Timeo VILLETTE <timeo.villette@viacesi.fr>
2024-06-17 16:49:45 +0200

    admin

--- frontend/ra-app/src/dataProvider.tsx
+++ frontend/ra-app/src/dataProvider.tsx
@@ -1,16 +1,38 @@
-const dataProvider = {
-    getList: (resource : string,) => {
-        return fetch(`http://localhost:8000/${resource}`)
-            .then(response => response.json())
-            .then(({ data }) => ({
-                data,
-                total: data.length,
-            }));
+import { DataProvider } from "react-admin"
+
+const transformData = (data: any) => {
+   const t = data.map((item: any)=>{
+        return {
+            ...item,
+            id: item._id ? item._id : item.id,
+        };
+    })
+    console.log(t)
+    return t
+};
+
+const dataProvider: DataProvider = {
+    getList: (resource : string, params:any) => {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify(params.filter),
+        };
+        return fetch(`api/${resource}?${new URLSearchParams(query)}`)
+            .then(response => {
+                return response.json()})
+            .then(( data ) => {
+                return {
+                data: transformData(data),
+                total: data?.length,
+            }});
     },
     getOne: (resource : string, params : any) => {
-        return fetch(`http://localhost:8000/${resource}/${params.id}`)
+        return fetch(`api/${resource}/${params.id}`)
             .then(response => response.json())
-            .then(({ data }) => ({
+            .then((data) => ({
                 data,
             }));
     },
@@ -18,9 +40,9 @@ const dataProvider = {
         const query = {
             filter: JSON.stringify({ id: params.ids }),
         };
-        return fetch(`http://localhost:8000/${resource}?${stringify(query)}`)
+        return fetch(`api/${resource}?${String(query)}`)
             .then(response => response.json())
-            .then(({ data }) => ({
+            .then((data) => ({
                 data,
             }));
     },
@@ -28,61 +50,65 @@ const dataProvider = {
         const query = {
             filter: JSON.stringify({ ...params.filter, [params.target]: params.id }),
         };
-        return fetch(`http://localhost:8000/${resource}?${stringify(query)}`)
+        return fetch(`api/${resource}?${String(query)}`)
             .then(response => response.json())
-            .then(({ data }) => ({
+            .then((data) => ({
                 data,
                 total: data.length,
             }));
     },
     update: (resource : string, params : any) =>
-        fetch(`http://localhost:8000/${resource}/${params.id}`, {
+        fetch(`api/${resource}/${params.id}`, {
             method: 'PUT',
             body: JSON.stringify(params.data),
+            headers: new Headers({
+                'Content-Type': 'application/json', // Change the content type here
+                'Accept': 'application/json',
+            })
         })
             .then(response => response.json())
-            .then(({ data }) => ({
+            .then((data) => ({
                 data,
             })),
     updateMany: (resource : string, params : any) => {
         const query = {
             filter: JSON.stringify({ id: params.ids }),
         };
-        return fetch(`http://localhost:8000/${resource}?${stringify(query)}`, {
+        return fetch(`api/${resource}?${String(query)}`, {
             method: 'PUT',
             body: JSON.stringify(params.data),
         })
             .then(response => response.json())
-            .then(({ data }) => ({
+            .then((data) => ({
                 data,
             }));
     },
     create: (resource : string, params : any) =>
-        fetch(`http://localhost:8000/${resource}`, {
+        fetch(`api/${resource}`, {
             method: 'POST',
             body: JSON.stringify(params.data),
         })
             .then(response => response.json())
-            .then(({ data }) => ({
+            .then((data) => ({
                 data,
             })),
     delete: (resource : string, params : any) =>
-        fetch(`http://localhost:8000/${resource}/${params.id}`, {
+        fetch(`api/${resource}/${params.id}`, {
             method: 'DELETE',
         })
             .then(response => response.json())
-            .then(({ data }) => ({
+            .then((data) => ({
                 data,
             })),
     deleteMany: (resource : string, params : any) => {
         const query = {
             filter: JSON.stringify({ id: params.ids }),
         };
-        return fetch(`http://localhost:8000/${resource}?${stringify(query)}`, {
+        return fetch(`api/${resource}?${String(query)}`, {
             method: 'DELETE',
         })
             .then(response => response.json())
-            .then(({ data }) => ({
+            .then((data) => ({
                 data,
             }));
     }
