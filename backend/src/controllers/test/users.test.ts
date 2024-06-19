9a617eaaf87dc17554e5b425d5bfe51433836746
Timeo VILLETTE <timeo.villette@viacesi.fr>
2024-06-19 11:03:17 +0200

    Admin panel

---
+++ backend/src/controllers/test/users.test.ts
@@ -0,0 +1,24 @@
+import { getAllUsers } from "controllers/users";
+import express from "express";
+import request from "supertest";
+
+jest.mock("../../db/users");
+
+describe("getAllUsers", () => {
+    it("should return 200 and all users when users exist", async () => {
+        const mockUsers = [
+          { id: 1, username: "user1" },
+          { id: 2, username: "user2" },
+        ];
+        (getAllUsers as jest.Mock).mockResolvedValue(mockUsers);
+    
+        const app = express();
+        app.get("/users", getAllUsers);
+    
+        const res = await request(app).get("/users");
+    
+        expect(res.statusCode).toEqual(200);
+        expect(res.body).toEqual(mockUsers);
+    
+})
+})
