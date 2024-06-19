bbd4ab248311480a6586dbb20211b8309bb140e3
Timeo VILLETTE <timeo.villette@viacesi.fr>
2024-06-19 15:42:50 +0200

    Test user + Dev --> Swagger doc

--- backend/src/controllers/test/users.test.ts
+++ backend/src/controllers/test/users.test.ts
@@ -1,24 +1,31 @@
-import { getAllUsers } from "../users";
+import { getAUserById } from "../users";
+import { getUserById } from "../../db/users";
 import express from "express";
 import request from "supertest";
 
 jest.mock("../../db/users");
 
-describe("getAllUsers", () => {
-    it("should return 200 and all users when users exist", async () => {
-        const mockUsers = [
-          { id: 1, username: "user1" },
-          { id: 2, username: "user2" },
-        ];
-        (getAllUsers as jest.Mock).mockResolvedValue(mockUsers);
-    
+describe("getAUserById", () => {
+    it("should return 200 and the user when the user exists", async () => {
+        const mockUser = { id: 1, username: "user1" };
+        (getUserById as jest.Mock).mockResolvedValue(mockUser);
+
         const app = express();
-        app.get("/users", getAllUsers);
-    
-        const res = await request(app).get("/users");
-    
+        app.get("/users/:id", getAUserById);
+
+        const res = await request(app).get("/users/1");
+
         expect(res.statusCode).toEqual(200);
-        expect(res.body).toEqual(mockUsers);
-    
-})
-})
+        expect(res.body).toEqual(mockUser);
+    });
+    it("should return 404 when the user does not exist", async () => {
+        (getUserById as jest.Mock).mockResolvedValue(null);
+
+        const app = express();
+        app.get("/users/:id", getAUserById);
+
+        const res = await request(app).get("/users/1");
+
+        expect(res.statusCode).toEqual(404);
+    });
+});
--- frontend/foodcesi/src/pages/home/HomeDeveloper.tsx
+++ frontend/foodcesi/src/pages/home/HomeDeveloper.tsx
@@ -1,21 +1,8 @@
 import { Button } from "@/components/ui/button";
-// import {
-//     DropdownMenu,
-//     DropdownMenuContent,
-//     DropdownMenuRadioGroup,
-//     DropdownMenuRadioItem,
-//     DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu";
 import { useAuth } from "@/context/AuthContext";
 import axios from "axios";
-// import { ChevronDown } from "lucide-react";
 import { useEffect, useState } from "react";
-// import { BarChart } from "@mui/x-charts/BarChart";
-
-// const droits = [
-//     { id: 1, name: "rggsegsge" },
-//     { id: 2, name: "Travail" },
-// ];
+import { NavLink } from "react-router-dom";
 
 export default function HomeDeveloper() {
     const { user } = useAuth();
@@ -58,73 +45,25 @@ export default function HomeDeveloper() {
         return undefined;
     }
 
-    // const [selectedDroit, setSelectedDroit] = useState(droits[0]);
-
     if (!key) {
         return (
             <div className="flex flex-col items-center justify-center gap-2">
                 <h1 className="text-lg font-semibold text-nowrap">
                     Vous n'avez pas encore de clé de sécurité ?
                 </h1>
-                {/* Sélectionner vos droits :
-                <DropdownMenu>
-                    <DropdownMenuTrigger asChild>
-                        <Button
-                            variant="white"
-                            className="flex items-center justify-center gap-2 px-0 text-grey"
-                        >
-                            <span>{selectedDroit.name}</span>
-                            <ChevronDown size="16" />
-                        </Button>
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent className="w-56">
-                        <DropdownMenuRadioGroup
-                            value={selectedDroit.name}
-                            onValueChange={(value) =>
-                                setSelectedDroit(
-                                    droits.find(
-                                        (address) => address.name === value
-                                    ) || selectedDroit
-                                )
-                            }
-                        >
-                            {droits.map((droit, index) => (
-                                <DropdownMenuRadioItem
-                                    key={index}
-                                    value={droit.name}
-                                >
-                                    {droit.name}
-                                </DropdownMenuRadioItem>
-                            ))}
-                        </DropdownMenuRadioGroup>
-                    </DropdownMenuContent>
-                </DropdownMenu> */}
                 <Button onClick={onClickCreateKey}>Faire la demande</Button>
             </div>
         );
     } else {
         return (
             <div className="flex flex-col items-center justify-center gap-2">
-                {/* <h2>Autorisation : Admin</h2> */}
                 <h2>Clé : {key} </h2>
                 <Button variant="link" className="underline" onClick={onClickDeleteKey}>
                     Supprimer la clé
                 </Button>
-                <Button>Consulter la documentation</Button>
-                <h2>Utilisation ces derniers mois :</h2>
-                {/* <BarChart
-                    series={[
-                        { data: [35, 44, 24, 34] },
-                        { data: [51, 6, 49, 30] },
-                        { data: [15, 25, 30, 50] },
-                        { data: [60, 50, 15, 25] },
-                    ]}
-                    height={290}
-                    xAxis={[
-                        { data: ["Q1", "Q2", "Q3", "Q4"], scaleType: "band" },
-                    ]}
-                    margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
-                /> */}
+                <a href="/api/api-docs/" target="_blank" rel="noopener noreferrer">
+                        <Button>Consulter la documentation</Button>
+                    </a>
             </div>
         );
     }
