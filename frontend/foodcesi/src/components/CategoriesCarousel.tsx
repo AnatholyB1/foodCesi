e440238288739930801439f34467d35d5376c90c
Antoine Favereau <antoinefavereau45@gmail.com>
2024-06-21 00:36:56 +0200

    feat: categories carousel

--- frontend/foodcesi/src/components/CategoriesCarousel.tsx
+++ frontend/foodcesi/src/components/CategoriesCarousel.tsx
@@ -1,31 +1,35 @@
+import { useEffect, useState } from "react";
 import CustomCard from "./ui/CustomCard";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
-
-interface Category {
-    id: number;
-    name: string;
-    icon: string;
-}
-
-const categories: Category[] = [
-    { id: 1, name: "Burger", icon: "/categoryIcons/Burger.svg" },
-    { id: 2, name: "Pizza", icon: "/categoryIcons/Pizza.svg" },
-    { id: 3, name: "Frites", icon: "/categoryIcons/Frites.svg" },
-    { id: 4, name: "Hot-dog", icon: "/categoryIcons/Hot-dog.svg" },
-    { id: 5, name: "Pates", icon: "/categoryIcons/Pates.svg" },
-    { id: 6, name: "Tacos", icon: "/categoryIcons/Tacos.svg" },
-    { id: 7, name: "Sushi", icon: "/categoryIcons/Sushi.svg" },
-    { id: 8, name: "Nouilles", icon: "/categoryIcons/Nouilles.svg" },
-];
+import api from "@/helpers/api";
+import { logError } from "@/helpers/utils";
 
 export default function CategoriesCarousel() {
+    const [categories, setCategories] = useState<RestaurantCategory[]>([]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await api.get(`/restaurant_categories`);
+
+                const data = response.data;
+                if (data.length > 0) {
+                    setCategories(data);
+                }
+            } catch (error: any) {
+                logError(error);
+            }
+        };
+        fetchCategories();
+    }, []);
+
     return (
         <Carousel className="w-full">
             <CarouselContent>
                 {categories.map((category) => (
                     <CarouselItem key={category.id} className="basis-1/5 md:basis-1/6">
                         <CustomCard className="flex flex-col items-center gap-1 rounded-2xl p-4">
-                            <img src={category.icon} alt={category.name} />
+                            <img src={`/categoryIcons/${category.name}.svg`} alt={category.name} />
                             <p className="text-xs text-nowrap">{category.name}</p>
                         </CustomCard>
                     </CarouselItem>
