595fb53e5e10899b156a1e6dafcbfff1ac72f31c
Antoine Favereau <antoinefavereau45@gmail.com>
2024-06-20 13:05:36 +0200

    fix(frontend): fix ref warning

--- frontend/foodcesi/src/components/ui/AccountItem.tsx
+++ frontend/foodcesi/src/components/ui/AccountItem.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { ChevronRight } from "lucide-react";
 import CustomCard from "./CustomCard";
+import { forwardRef } from "react";
 
 interface AccountItemProps {
     icon: JSX.Element;
@@ -9,16 +10,17 @@ interface AccountItemProps {
     action?: () => void;
 }
 
-const AccountItem = ({ variant, icon, title, action }: AccountItemProps) => {
+const AccountItem = forwardRef((props: AccountItemProps, ref: React.Ref<HTMLButtonElement>) => {
+    const { variant, icon, title, action } = props;
     return (
         <CustomCard className={cn("w-full", { "bg-primary": variant === "primary" })}>
-            <button type="button" className={cn("w-full flex items-center gap-4 p-4 text-primary", { "text-white": variant === "primary" })} onClick={action}>
+            <button ref={ref} type="button" className={cn("w-full flex items-center gap-4 p-4 text-primary", { "text-white": variant === "primary" })} onClick={action}>
                 {icon}
                 <p className={cn("grow text-left text-black font-semibold", { "text-white": variant === "primary" })}>{title}</p>
                 <ChevronRight />
             </button>
         </CustomCard>
     );
-};
+});
 
 export default AccountItem;
