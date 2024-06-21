38bed60c2f3dd4839b6e6b75aed2bc19de265a79
Antoine Favereau <antoinefavereau45@gmail.com>
2024-06-21 02:03:49 +0200

    feat: wip categories filter

--- frontend/foodcesi/src/components/CategoriesCarousel.tsx
+++ frontend/foodcesi/src/components/CategoriesCarousel.tsx
@@ -3,8 +3,15 @@ import CustomCard from "./ui/CustomCard";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import api from "@/helpers/api";
 import { logError } from "@/helpers/utils";
+import { useNavigate } from "react-router-dom";
 
-export default function CategoriesCarousel() {
+interface CategoryCarouselProps {
+    onChange?: (category: number[]) => void;
+}
+
+export default function CategoriesCarousel({ onChange }: CategoryCarouselProps) {
+    const navigate = useNavigate();
+
     const [categories, setCategories] = useState<RestaurantCategory[]>([]);
 
     useEffect(() => {
@@ -27,7 +34,7 @@ export default function CategoriesCarousel() {
         <Carousel className="w-full">
             <CarouselContent>
                 {categories.map((category) => (
-                    <CarouselItem key={category.id} className="basis-1/5 md:basis-1/6">
+                    <CarouselItem key={category.id} className="basis-1/5 md:basis-1/6" onClick={() => (onChange ? onChange([category.id || 0]) : navigate(`/recherche?filter=${encodeURIComponent(JSON.stringify({ categories: [category.id || 0] }))} `))}>
                         <CustomCard className="flex flex-col items-center gap-1 rounded-2xl p-4">
                             <img src={`/categoryIcons/${category.name}.svg`} alt={category.name} />
                             <p className="text-xs text-nowrap">{category.name}</p>
